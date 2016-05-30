3e74823aea384fce55c7d92f3ead55ef81a54ea9
Samuel Wein <poshul@gmail.com>
2016-05-30 14:23:07 -0400

    Cleaned up PreprocessSpectra, added decharging precursor scans according to metadata

---
+++ src/utils/PreprocessSpectra.cpp
@@ -0,0 +1,388 @@
+﻿// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2016.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Timo Sachsenberg $
+// $Authors: Timo Sachsenberg $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/KERNEL/StandardTypes.h>
+#include <OpenMS/FORMAT/MzMLFile.h>
+#include <OpenMS/CONCEPT/Constants.h>
+#include <OpenMS/APPLICATIONS/TOPPBase.h>
+#include <OpenMS/DATASTRUCTURES/Param.h>
+#include <OpenMS/KERNEL/MSSpectrum.h>
+#include <OpenMS/METADATA/SpectrumSettings.h>
+#include <OpenMS/KERNEL/MSExperiment.h>
+#include <OpenMS/FORMAT/MzMLFile.h>
+#include <OpenMS/FORMAT/FASTAFile.h>
+#include <OpenMS/CHEMISTRY/EnzymaticDigestion.h>
+#include <OpenMS/CHEMISTRY/EnzymesDB.h>
+
+#include <OpenMS/CHEMISTRY/ModificationsDB.h>
+#include <OpenMS/ANALYSIS/RNPXL/ModifiedPeptideGenerator.h>
+
+// preprocessing and filtering
+#include <OpenMS/FILTERING/TRANSFORMERS/ThresholdMower.h>
+#include <OpenMS/FILTERING/TRANSFORMERS/NLargest.h>
+#include <OpenMS/FILTERING/TRANSFORMERS/WindowMower.h>
+#include <OpenMS/FILTERING/TRANSFORMERS/Normalizer.h>
+
+#include <OpenMS/CHEMISTRY/TheoreticalSpectrumGenerator.h>
+#include <OpenMS/KERNEL/Peak1D.h>
+#include <OpenMS/KERNEL/RichPeak1D.h>
+#include <OpenMS/FORMAT/IdXMLFile.h>
+
+#include <OpenMS/CHEMISTRY/ModificationsDB.h>
+#include <OpenMS/CHEMISTRY/ResidueModification.h>
+
+#include <OpenMS/FILTERING/ID/IDFilter.h>
+
+#include <map>
+#include <algorithm>
+
+#ifdef _OPENMP
+  #include <omp.h>
+  #define NUMBER_OF_THREADS (omp_get_num_threads())
+#else
+  #define NUMBER_OF_THREADS (1)
+#endif
+
+
+using namespace OpenMS;
+using namespace std;
+
+/*
+  TODO:
+   - proper C-term N-term handling of terminal modifications that can be at every amino acid
+
+        // should be something like this: check if AA of modification and peptide match
+        if (origin != aa_seq[pos].getOneLetterCode() && origin != "C-term" && origin != "N-term")
+        {
+          continue;
+        }
+
+  // check for common annotation error in unimod
+  if ((origin == "C-term" || origin == "N-term") && term_specifity == ResidueModification::ANYWHERE)
+        {
+          continue;
+        }
+
+   - make hyperscore scoring linear
+   - single and multiple neutral loss spectra creation
+*/
+
+class PreprocessSpectra :
+    public TOPPBase
+{
+  public:
+    PreprocessSpectra() :
+      TOPPBase("PreprocessSpectra", "Mows, decharges and deisotopes spectra", false)
+    {
+    }
+
+  protected:
+    void registerOptionsAndFlags_()
+    {
+      registerInputFile_("in", "<file>", "", "input file ");
+      setValidFormats_("in", ListUtils::create<String>("mzML"));
+
+      registerOutputFile_("out", "<file>", "", "output file ");
+      setValidFormats_("out", ListUtils::create<String>("mzML"));
+
+      registerTOPPSubsection_("precursor", "Precursor (Parent Ion) Options");
+      registerDoubleOption_("precursor:mass_tolerance", "<tolerance>", 10.0, "Width of precursor mass tolerance window", false);
+
+      StringList precursor_mass_tolerance_unit_valid_strings;
+      precursor_mass_tolerance_unit_valid_strings.push_back("ppm");
+      precursor_mass_tolerance_unit_valid_strings.push_back("Da");
+
+      registerStringOption_("precursor:mass_tolerance_unit", "<unit>", "ppm", "Unit of precursor mass tolerance.", false, false);
+      setValidStrings_("precursor:mass_tolerance_unit", precursor_mass_tolerance_unit_valid_strings);
+
+      registerIntOption_("precursor:min_charge", "<num>", 2, "Minimum precursor charge to be considered.", false, true);
+      registerIntOption_("precursor:max_charge", "<num>", 5, "Maximum precursor charge to be considered.", false, true);
+
+      registerIntOption_("min_isopeaks", "<num>", 3, "Minimum number of isotopes to be considered.", false, true);
+      registerIntOption_("max_isopeaks", "<num>", 10, "Maximum number of isotopes to be considered.", false, true);
+
+
+      registerTOPPSubsection_("fragment", "Fragments (Product Ion) Options");
+      registerDoubleOption_("fragment:mass_tolerance", "<tolerance>", 10.0, "Fragment mass tolerance", false);
+
+      StringList fragment_mass_tolerance_unit_valid_strings;
+      fragment_mass_tolerance_unit_valid_strings.push_back("ppm");
+      fragment_mass_tolerance_unit_valid_strings.push_back("Da");
+
+      registerStringOption_("fragment:mass_tolerance_unit", "<unit>", "ppm", "Unit of fragment m", false, false);
+      setValidStrings_("fragment:mass_tolerance_unit", fragment_mass_tolerance_unit_valid_strings);
+
+    }
+
+
+    // spectrum must not contain 0 intensity peaks and must be sorted by m/z
+    template <typename SpectrumType>
+    static void deisotopeAndSingleChargeMSSpectrum(SpectrumType& in, Int min_charge, Int max_charge, double fragment_tolerance, bool fragment_unit_ppm, bool keep_only_deisotoped = false, Size min_isopeaks = 3, Size max_isopeaks = 10, bool make_single_charged = true)
+    {
+      if (in.empty())
+      {
+        return;
+      }
+
+      SpectrumType old_spectrum = in;
+
+      // determine charge seeds and extend them
+      vector<Size> mono_isotopic_peak(old_spectrum.size(), 0);
+      vector<Int> features(old_spectrum.size(), -1);
+      Int feature_number = 0;
+
+      for (Size current_peak = 0; current_peak != old_spectrum.size(); ++current_peak)
+      {
+        double current_mz = old_spectrum[current_peak].getPosition()[0];
+
+        for (Int q = max_charge; q >= min_charge; --q)   // important: test charge hypothesis from high to low
+        {
+          // try to extend isotopes from mono-isotopic peak
+          // if extension larger then min_isopeaks possible:
+          //   - save charge q in mono_isotopic_peak[]
+          //   - annotate all isotopic peaks with feature number
+          if (features[current_peak] == -1)   // only process peaks which have no assigned feature number
+          {
+            bool has_min_isopeaks = true;
+            vector<Size> extensions;
+            for (Size i = 0; i < max_isopeaks; ++i)
+            {
+              double expected_mz = current_mz + i * Constants::C13C12_MASSDIFF_U / q;
+              Size p = old_spectrum.findNearest(expected_mz);
+              double tolerance_dalton = fragment_unit_ppm ? fragment_tolerance * old_spectrum[p].getPosition()[0] * 1e-6 : fragment_tolerance;
+              if (fabs(old_spectrum[p].getPosition()[0] - expected_mz) > tolerance_dalton)   // test for missing peak
+              {
+                if (i < min_isopeaks)
+                {
+                  has_min_isopeaks = false;
+                }
+                break;
+              }
+              else
+              {
+                // TODO: include proper averagine model filtering. for now start at the second peak to test hypothesis
+                Size n_extensions = extensions.size();
+                if (n_extensions != 0)
+                {
+                  if (old_spectrum[p].getIntensity() > old_spectrum[extensions[n_extensions - 1]].getIntensity())
+                  {
+                    if (i < min_isopeaks)
+                    {
+                      has_min_isopeaks = false;
+                    }
+                    break;
+                  }
+                }
+
+                // averagine check passed
+                extensions.push_back(p);
+              }
+            }
+
+            if (has_min_isopeaks)
+            {
+              //cout << "min peaks at " << current_mz << " " << " extensions: " << extensions.size() << endl;
+              mono_isotopic_peak[current_peak] = q;
+              for (Size i = 0; i != extensions.size(); ++i)
+              {
+                features[extensions[i]] = feature_number;
+              }
+              feature_number++;
+            }
+          }
+        }
+      }
+
+      in.clear(false);
+      for (Size i = 0; i != old_spectrum.size(); ++i)
+      {
+        Int z = mono_isotopic_peak[i];
+        if (keep_only_deisotoped)
+        {
+          if (z == 0)
+          {
+            continue;
+          }
+
+          // if already single charged or no decharging selected keep peak as it is
+          if (!make_single_charged)
+          {
+            in.push_back(old_spectrum[i]);
+          }
+          else
+          {
+            Peak1D p = old_spectrum[i];
+            p.setMZ(p.getMZ() * z - (z - 1) * Constants::PROTON_MASS_U);
+            in.push_back(p);
+          }
+        }
+        else
+        {
+          // keep all unassigned peaks
+          if (features[i] < 0)
+          {
+            in.push_back(old_spectrum[i]);
+            continue;
+          }
+
+          // convert mono-isotopic peak with charge assigned by deisotoping
+          if (z != 0)
+          {
+            if (!make_single_charged)
+            {
+              in.push_back(old_spectrum[i]);
+            }
+            else
+            {
+              Peak1D p = old_spectrum[i];
+              p.setMZ(p.getMZ() * z - (z - 1) * Constants::PROTON_MASS_U);
+              in.push_back(p);
+            }
+          }
+        }
+      }
+
+      in.sortByPosition();
+    }
+
+    double logfactorial(UInt x)
+    {
+      UInt y;
+
+      if (x < 2)
+        return 1;
+      else
+      {
+        double z = 0;
+        for (y = 2; y <= x; y++)
+        {
+          z = log((double)y) + z;
+        }
+
+        return z;
+      }
+    }
+
+
+    void preprocessSpectra_(PeakMap& exp, double fragment_mass_tolerance, bool fragment_mass_tolerance_unit_ppm, Int min_charge = 1, Int max_charge = 3, Size min_isopeaks = 3, Size max_isopeaks = 10)
+    {
+      // filter MS2 map
+      // remove 0 intensities
+      ThresholdMower threshold_mower_filter;
+      threshold_mower_filter.filterPeakMap(exp);
+
+      Normalizer normalizer;
+      normalizer.filterPeakMap(exp);
+
+      // sort by rt
+      exp.sortSpectra(false);
+
+      // filter settings
+      WindowMower window_mower_filter;
+      Param filter_param = window_mower_filter.getParameters();
+      filter_param.setValue("windowsize", 100.0, "The size of the sliding window along the m/z axis.");
+      filter_param.setValue("peakcount", 20, "The number of peaks that should be kept.");
+      filter_param.setValue("movetype", "jump", "Whether sliding window (one peak steps) or jumping window (window size steps) should be used.");
+      window_mower_filter.setParameters(filter_param);
+
+      NLargest nlargest_filter = NLargest(400);
+
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+      for (SignedSize exp_index = 0; exp_index < (SignedSize)exp.size(); ++exp_index)
+      {
+        // sort by mz
+        exp[exp_index].sortByPosition();
+
+        // deisotope
+        deisotopeAndSingleChargeMSSpectrum(exp[exp_index], min_charge, max_charge, fragment_mass_tolerance, fragment_mass_tolerance_unit_ppm, false, min_isopeaks, max_isopeaks, true);
+
+        // remove noise
+        window_mower_filter.filterPeakSpectrum(exp[exp_index]);
+        nlargest_filter.filterPeakSpectrum(exp[exp_index]);
+
+        // sort (nlargest changes order)
+        exp[exp_index].sortByPosition();
+      }
+    }
+
+
+    ExitCodes main_(int, const char**)
+    {
+      ProgressLogger progresslogger;
+      progresslogger.setLogType(log_type_);
+      String in_mzml = getStringOption_("in");
+      String out_mzml = getStringOption_("out");
+
+      Int min_precursor_charge = getIntOption_("precursor:min_charge");
+      Int max_precursor_charge = getIntOption_("precursor:max_charge");
+      Int min_isopeaks = getIntOption_("min_isopeaks");
+      Int max_isopeaks = getIntOption_("max_isopeaks");
+      bool make_single_charged = (getParamAsBool_("make_single_charged"));
+      double precursor_mass_tolerance = getDoubleOption_("precursor:mass_tolerance");
+      bool precursor_mass_tolerance_unit_ppm = (getStringOption_("precursor:mass_tolerance_unit") == "ppm");
+
+      double fragment_mass_tolerance = getDoubleOption_("fragment:mass_tolerance");
+      bool fragment_mass_tolerance_unit_ppm = (getStringOption_("fragment:mass_tolerance_unit") == "ppm");
+
+      // load MS2 map
+      PeakMap spectra;
+      MzMLFile f;
+      f.setLogType(log_type_);
+
+      PeakFileOptions options;
+      options.clearMSLevels();
+      options.addMSLevel(2);
+      f.getOptions() = options;
+      f.load(in_mzml, spectra);
+      spectra.sortSpectra(true);
+
+      progresslogger.startProgress(0, 1, "Filtering spectra...");
+      preprocessSpectra_(spectra, fragment_mass_tolerance, fragment_mass_tolerance_unit_ppm, min_precursor_charge, max_precursor_charge, min_isopeaks, max_isopeaks);
+      progresslogger.endProgress();
+
+      //output mzML
+      IdXMLFile().store(out_mzml, protein_ids, peptide_ids); //FIXME
+
+      return EXECUTION_OK;
+    }
+
+};
+
+int main(int argc, const char** argv)
+{
+  PreprocessSpectra tool;
+  return tool.main(argc, argv);
+}
